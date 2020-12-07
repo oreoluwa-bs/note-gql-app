b4aa7761471a9f676ad80d23b3ff36e0d5892c4e
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-12-07 02:22:22 +0100

    Fixed Login Page

--- src/views/Main/HomePage.jsx
+++ src/views/Main/HomePage.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Logo from '../../assets/images/Logo.svg';
 
 const HomePage = () => {
   return (
     <div>
-      <div style={{ minHeight: '100vh' }}>
-        <div
+      <div className="home">
+        <header className="header">
+          <nav className="navbar">
+            <img src={Logo} alt="Logo" className="logo" />
+          </nav>
+        </header>
+        <div className="hero">
+          <div className="hero__content">
+            <span>
+              <h1 className="hero__header">Note taking made easy and safe</h1>
+              <p className="hero__subhead">
+                The quick, brown fox jumps over a lazy dog. DJs flock by when
+                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
+                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
+                Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt
+                fox. Bright vixens jump; dozy fowl quack.
+              </p>
+              <button className="btn btn__primary">Get Started</button>
+            </span>
+          </div>
+        </div>
+        {/* <div
           className="hero"
           style={{ display: 'flex', justifyContent: 'space-between' }}>
           <div>
@@ -24,9 +45,9 @@ const HomePage = () => {
               src={require('../../assets/images/hero__image.jpeg').default}
               alt="Laptop with note application screen"
               className="hero__image"
-            /> */}
+            /> 
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
