72267ddc134d2960b0145e8f695a7f9e14e2082f
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-12-12 00:27:13 +0100

    Moved gql queries to single location

---
+++ src/store/schema/noteQueries.js
@@ -0,0 +1,44 @@
+const { gql } = require('@apollo/client');
+
+export const GET_MY_NOTES = gql`
+  query GetMyNotes($author: MongoID!) {
+    getNotes(filter: { author: $author }) {
+      _id
+      title
+      slug
+      content
+      author {
+        _id
+      }
+    }
+  }
+`;
+
+export const GET_MY_NOTE = gql`
+  query GetMyNote($slug: String!) {
+    getNoteByField(filter: { slug: $slug }) {
+      _id
+      title
+      slug
+      content
+      author {
+        _id
+      }
+    }
+  }
+`;
+
+export const CREATE_NOTE = gql`
+  mutation CreateNote($content: String!, $author: MongoID!) {
+    createNote(record: { content: $content, author: $author }) {
+      recordId
+      record {
+        _id
+        title
+        content
+        slug
+        createdAt
+      }
+    }
+  }
+`;
